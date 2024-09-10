a8cd60ddfdcb2117e9c7908cb86c8a90bcc9c36d
milquejas <84683894+milquejas@users.noreply.github.com>
2024-09-10 14:51:05 +0300

    Rautalankamalli
    Nyt Sign in valmiina.. Ulos ei pysty kirjautumaan kun kerran on kirjautunut sisään.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View} from 'react-native'
-import {SplashScreen, Stack} from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native'
+import { SplashScreen, Stack } from 'expo-router';
 import { useFonts } from 'expo-font'
 import { useEffect } from 'react';
+import GlobalProvider from '../context/GlobalProvider';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -17,26 +18,29 @@ const RootLayout = () => {
     "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
     "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   });
-  
-useEffect(() => {
-  if (error) throw error;
 
-  if (fontsLoaded) {
-    SplashScreen.hideAsync();
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
   }
-}, [fontsLoaded, error]);
-
-if (!fontsLoaded && !error) {
-  return null;
-}
   return (
-  <Stack>
-  <Stack.Screen name ="index" options = {{ headerShown: false}}/>
-  <Stack.Screen name ="(auth)" options = {{ headerShown: false}}/>
-  <Stack.Screen name ="(tabs)" options = {{ headerShown: false}}/>
-  {/* <Stack.Screen name ="/search/[query]" options = {{ headerShown: false}}/> */}
-  </Stack>
-  ) 
+    <GlobalProvider>
+
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name ="/search/[query]" options = {{ headerShown: false}}/> */}
+      </Stack>
+    </GlobalProvider>
+  )
 }
 
 export default RootLayout
